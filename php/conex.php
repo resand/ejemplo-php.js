78fa568c07b0266c520f9b22672c7fc437b45eb1
René Sandoval <resand91@gmail.com>
2014-11-10 17:56:18 -0600

    Revert "fix función conectar"
    This reverts commit 7f352ddfb992f948ae0b9c6547505c3923234bd5.

--- php/conex.php
+++ php/conex.php
@@ -1,25 +1,16 @@
 <?
-// //Variables para conectarse al servidor
-// $HOSTNAME = "localhost"; 		//SERVIDOR   
-// $USERNAME = "root";	     		//USUARIO
-// $PASSWORD = "root";	     		//CONTRASENIA
-// $DATABASE = "inventarios";		//BASE DE DATOS
+//Variables para conectarse al servidor
+$HOSTNAME = "localhost"; 		//SERVIDOR   
+$USERNAME = "root";	     		//USUARIO
+$PASSWORD = "root";	     		//CONTRASENIA
+$DATABASE = "inventarios";		//BASE DE DATOS
 
-
-// function conectar(){
-//   global $HOSTNAME, $USERNAME, $PASSWORD, $DATABASE;
-//   $idcnx = mysql_connect($HOSTNAME, $USERNAME, $PASSWORD) or
-// 	DIE(mysql_error());
-//   mysql_select_db ($DATABASE, $idcnx);
-//   return $idcnx;
-//  }
 
 function conectar(){
- 	$con = mysql_connect("localhost","root","root");
- 	if (!$con){
-  		die('Could not connect: ' . mysql_error());
+  global $HOSTNAME, $USERNAME, $PASSWORD, $DATABASE;
+  $idcnx = mysql_connect($HOSTNAME, $USERNAME, $PASSWORD) or
+	DIE(mysql_error());
+  mysql_select_db ($DATABASE, $idcnx);
+  return $idcnx;
  }
- mysql_select_db("inventarios", $con);
- return $con;
-}
 ?>
