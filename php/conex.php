7f352ddfb992f948ae0b9c6547505c3923234bd5
René Sandoval <resand91@gmail.com>
2014-11-10 17:53:35 -0600

    fix función conectar

--- php/conex.php
+++ php/conex.php
@@ -1,16 +1,25 @@
 <?
-//Variables para conectarse al servidor
-$HOSTNAME = "localhost"; 		//SERVIDOR   
-$USERNAME = "root";	     		//USUARIO
-$PASSWORD = "root";	     		//CONTRASENIA
-$DATABASE = "inventarios";		//BASE DE DATOS
+// //Variables para conectarse al servidor
+// $HOSTNAME = "localhost"; 		//SERVIDOR   
+// $USERNAME = "root";	     		//USUARIO
+// $PASSWORD = "root";	     		//CONTRASENIA
+// $DATABASE = "inventarios";		//BASE DE DATOS
 
+
+// function conectar(){
+//   global $HOSTNAME, $USERNAME, $PASSWORD, $DATABASE;
+//   $idcnx = mysql_connect($HOSTNAME, $USERNAME, $PASSWORD) or
+// 	DIE(mysql_error());
+//   mysql_select_db ($DATABASE, $idcnx);
+//   return $idcnx;
+//  }
 
 function conectar(){
-  global $HOSTNAME, $USERNAME, $PASSWORD, $DATABASE;
-  $idcnx = mysql_connect($HOSTNAME, $USERNAME, $PASSWORD) or
-	DIE(mysql_error());
-  mysql_select_db ($DATABASE, $idcnx);
-  return $idcnx;
+ 	$con = mysql_connect("localhost","root","root");
+ 	if (!$con){
+  		die('Could not connect: ' . mysql_error());
  }
+ mysql_select_db("inventarios", $con);
+ return $con;
+}
 ?>
